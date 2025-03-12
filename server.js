684f60f38e1c681ee1737a9e7603723d2fa864b5
whizshop <treyblair585@gmail.com>
2025-03-12 12:27:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
